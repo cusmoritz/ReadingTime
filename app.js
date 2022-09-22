7e33ead9f7bf5ff1bcf08e11ce4914b5f463b47d
cusmoritz <cus.moritz@gmail.com>
2022-09-21 19:22:59 -0600

    adding css and some js

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 // Init
 
 const headerText = document.getElementById('header-text');
-
+const mainBody = document.getElementById('main-body');
 
 headerText.addEventListener('click', () => {
     // re-load reading.time
 })
+
+let mainContentTable = document.createElement('table');
+mainBody.appendChild(mainContentTable);
+
+let numOfRows = 5;
+const createMainContent = (numOfRows) => {
+
+    // this is where we populate our main content with how many pages we pull
+    for (rowsInTable = 0; rowsInTable < numOfRows; rowsInTable++){
+        // create rows
+        let newRow = document.createElement('tr');
+
+        //create rows per input number
+
+        for (let cellsInRow = 0; cellsInRow < 3; cellsInRow++){
+            //create cells
+            let newCell = document.createElement('td');
+            // add cells to rows
+            newRow.appendChild(newCell);      
+        }
+    // add rows to board
+    mainContentTable.appendChild(newRow);
+    }
+    
+    return;
+}
+
+createMainContent();
+
+
+
+// create one row
+    // create 3 cells per row
 
 // we want a side bar (static)
 // with stats
