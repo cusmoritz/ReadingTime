86300c2c7569c2b2246a58456c6e2a338a5c89d7
cusmoritz <cus.moritz@gmail.com>
2022-09-21 18:44:50 -0600

    adding little html

---
+++ app.js
@@ -0,0 +1,24 @@
+// Init
+
+const headerText = document.getElementById('header-text');
+
+
+headerText.addEventListener('click', () => {
+    // re-load reading.time
+})
+
+// we want a side bar (static)
+// with stats
+    // how many articles
+    // total number of links clicked (?)
+    // other stuff?
+
+// on the main body of the page
+    // we want the table to populate with one thing at a time
+    // a headline from an API
+        // under that headline, the first paragraph (sentence) of text
+        // on the left (?) side of that, the logo from the website?
+        // header should be a link to the full story.
+
+    // we want to be able to pull tweets, story links from various websites ... etc
+    // ideally
